5fe2d71006fde50f7d0830e28d2a88b5317c7abd
hoya0326 <rbal5430@naver.com>
2026-08-22 03:38:38 +0900

    홈화면 지도 구현 완료

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,23 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+// 모든 하위 프로젝트 및 플러그인의 안드로이드 에스디케이(SDK) 버전을 강제로 36으로 고정합니다.
+// 평가 완료 여부를 확인하여 충돌 없이 설정을 주입하는 고도화된 방식입니다.
+subprojects {
+    val configureAndroid = {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.getByName("android")
+            if (android is com.android.build.gradle.BaseExtension) {
+                // 컴파일 및 타겟 에스디케이 버전을 36으로 강제 지정합니다.
+                android.compileSdkVersion(36)
+            }
+        }
+    }
+
+    if (project.state.executed) {
+        configureAndroid()
+    } else {
+        project.afterEvaluate { configureAndroid() }
+    }
+}
